959d903c42c5b5cf4e5beda901e9dd46891ddb93
OliverCH <694052985@qq.com>
2019-02-22 17:30:58 +0800

    the first commit

--- src/main/java/git_main.java
+++ src/main/java/git_main.java
@@ -1,5 +1,8 @@
 public class git_main {
     public static void main(String[] args) {
         System.out.println("这是主要的GIT");
+        System.out.println("Hello world");
+        System.out.println("http://5512aa.com");
+        System.out.println("pronhub");
     }
 }
